60225e373d8103551e1d54fadc1ded76b708bb32
dominique-thomas <52081569+dominique-thomas@users.noreply.github.com>
2024-01-23 17:35:31 -0600

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,111 @@
+let data = [
+
+    {
+        id:"ELEA_ASSE", 
+        keywords: ["3D Development", "Client Project", "Instructional Design"],
+        title: "eLearning Assets (Level 3 Interactivity)", 
+        year: "2022", 
+        role: "For this project, I helped develop 3D environments and render image stills, which were used in branching Practical Exercises (PEs).", 
+        scope: "Our team was tasked with developing eLearning content that supported <a href='https://community.articulate.com/articles/get-to-know-the-4-levels-of-e-learning' target='blank'>Level 3 Interactivity</a>. The client specified that the training material should depict the hardware using realistic 3D models, textures, and lighting. The target audience for this project was students ages 18 and up.", 
+        solution: "We used Technical Manuals and guidance from our Subject Matter Experts to ensure that our 3D environments accurately depicted Removal and Replacement Procedures. We also used accurate hardware specifications to ensure that everything that we modeled was correctly scaled.<br>The imagery that we rendered was used by developers to create branching PEs. In the PEs, students were required to use tools/parts located in the virtual bin. Students had to use the correct tool/part on the hardware prior to proceeding to the next step. Students were given tailored feedback depending on how they interacted with the hardware. Besides working on the 3D aspect, I also assisted in authoring several PE files for cross-training purposes.", 
+        specialNote: true,
+        imageRange: 2
+    },
+
+    {
+        id:"HAUN_HOUS", 
+        keywords: ["3D Development", "Personal Project", "Graphic Design"],
+        title: "Haunted House Title Sequence", 
+        year: "2022", 
+        role: "For this practice project, I invented a fictitious haunted house show The Tales of Moore Manor. During the project, I determined the goal, target audience, and product design.", 
+        scope: "I chose to make title sequence stills for a haunted house because I wanted more practice with low lighting and designing room interiors. The target audience for this project was adults ages 25 and up. In addition, this project also targeted consumers who prefer spooky and dark shows.", 
+        solution: "I spent a lot of my time planning the layout for the manor. Finding the right lighting also played a key role in setting the tone for the imagery. The haunted manor title stills were inspired by the show <i>Chapelwaite</i>. By the end of the project, I ended up creating several stills that leave the audience feeling a sense of emptiness and coldness.", 
+        specialNote: false,
+        imageRange: 4
+    },
+
+    {
+        id:"LUX_MOCK", 
+        title: "Luxury Brand Mockup", 
+        keywords: ["3D Development", "Personal Project", "Graphic Design"],
+        year: "2021", 
+        role: "For this practice project, I invented a luxury brand named Classic. During the project lifecycle, I determined the goal, target audience, and product design.", 
+        scope: "The target audience for this project was adults ages 20-40 years of age with mid-to high-income ranges. In addition, this project also targeted consumers who are considered fashion-forward and those who enjoy casual and sleek luxury brands.", 
+        solution: "I went through several design concepts to come up with the right look for the brand. I drew inspiration from popular brands such as Coach and Michel Kors. By the end of the project, I ended up depicting several casual-themed items using a leather texture to ensure they looked stylish and on par with modern luxury brands. I also created a unique logo to help emphasize its sleekness.", 
+        specialNote: false,
+        imageRange: 2
+    },
+
+    {
+        id:"ENER_MOCK", 
+        title: "Energy Drink Mockup", 
+        keywords: ["3D Development", "Personal Project", "Graphic Design"],
+        year: "2020", 
+        role: "For this practice project, I invented an energy drink brand named Rebound Energy. I determined the goal, target audience, brand styling, and advertising slogan during the project lifecycle.", 
+        scope: "I created this project because I wanted more practice creating 3D advertisements. I wanted to make an energy drink that looked modern and sleek. I targeted adults ages 18-35 years of age with low-income ranges. I also targeted consumers who are interested in basketball. I did this because there are not too many energy drinks that target this particular market. ", 
+        solution: "I went through several design iterations until I was satisfied with the result. I drew inspiration from popular energy-drink brands such as Red Bull and Monster Energy Drink. I chose to use a simple/clean background to pull the consumer into the brand. I also created a simple and stylish logo that helps convey the brand and its various flavors.", 
+        specialNote: false,
+        imageRange: 2
+    },
+
+    {
+        id:"VIRT_CLASS", 
+        title: "Manual Gunnery Video", 
+        keywords: ["3D Development", "Client Project", "Graphic Design", "Gamification", "Instructional Design"],
+        year: "2019", 
+        role: "For this project, I rendered still images to help create an interactive virtual classroom.", 
+        scope: "The client specified that they wanted a motivational piece added to the tactical software training course. They stated that it should also incorporate gamification techniques.", 
+        solution: "For the motivational piece, we decided to create a virtual classroom and incorporate a fun factor. The virtual classroom contained hotspots that allowed students to navigate and talk to simulated classmates. This classroom segment provided a unique motivational element to the training product that kept learners intrigued as they progressed through the training.", 
+        specialNote: true,
+        imageRange: 4
+    },
+
+    {
+        id:"VEHI_NAVI", 
+        title: "Manual Gunnery Video", 
+        keywords: ["3D Development", "Client Project", "Graphic Design", "Instructional Design"],
+        year: "2016", 
+        role: "I worked with a team of developers to set up cameras and render stills of tactical equipment to create an interactive navigational screen.", 
+        scope: "This navigational segment allowed students to navigate to different areas of the equipment. Moreover, it served as a way for students to identify areas of interest as they performed Removal and Replacement Procedures.", 
+        solution: "The training product was delivered using a virtual environment as it allowed us to provide on-demand training using real-world practice. One of the benefits of this project was that it reduced the likelihood of students breaking costly tools and equipment. Additionally, it provided realistic training to a number of students reducing the number of actual tools/equipment resources needed.", 
+        specialNote: true,
+        imageRange: 3
+    },
+
+    {
+        id:"INTRO_GUNN", 
+        title: "Manual Gunnery Video", 
+        year: "2015", 
+        keywords: ["Video Editing", "In-House Project", "Graphic Design", "Instructional Design"],
+        role: "I worked with a team of developers to render still images and animate cameras/3D models.", 
+        scope: "Our manager expressed the need to have a business development video that explains how to perform Manual Gunnery with the intent that a ballistic round hits a designated target. Our manager specified that this concept is often difficult to grasp for entry-level students.", 
+        solution: "We had a short turnaround for this project, therefore, we decided to repurpose an older 3D environment to speed up the development process. We also used a Technical Manual to ensure that we correctly depicted procedural steps. We demoed the project at business development conferences and during in-person tours of the company.", 
+        specialNote: true,
+        imageRange: 2
+    },
+
+    {
+        id:"FOUN_BALL", 
+        title: "Foundations of Ballistics Vignettes", 
+        keywords: ["3D Development", "In-House Project", "Video Editing", "Instructional Design"],
+        year: "2014", 
+        role: "I worked with other developers to set up the 3D environment, render still images, and animate several video vignettes. ", 
+        scope: "Our manager expressed the need to create video vignettes that explain the foundations of ballistics. In particular, our manager wanted us to incorporate 3D imagery and motion graphics to appeal to younger students ranging from ages 18-30.", 
+        solution: "The video vignettes illustrated some of the factors that affect a round's trajectory, such as target location, air density, and weapon condition (to name a few). We used Technical Manual specifications to help us create the 16 video vignettes. We demoed these vignettes at business development conferences, as well as during in-person company tours. The following images are stills from one vignette that covers a target's position.", 
+        specialNote: true,
+        imageRange: 3
+    },
+
+    {
+        id:"ANIM_CONT", 
+        title: "3D Animation Contest Entry", 
+        keywords: ["3D Development", "Personal Project"],
+        year: "2012", 
+        role: "I modeled, rigged, and animated 3D characters/environments for an animation contest.", 
+        scope: "I was required to create a 1-2 minute video that contained an original 3D character and 3D environment. For this contest, we were allowed to come up with our own stories. The target audience for the submission was ages 15 and older.", 
+        solution: "My story was about a family of ants that were traveling to the other side of the park. The family is faced with an obstacle along the way and attempts to remove it using comical means. I purposely made the ants smooth and friendly looking to make them more appealing to a younger audience. I also used a motion capturing (Mocap) system to help record movements for the 3D models. I won first place in the animation contest and I was hired as a 3D Developer intern.", 
+        specialNote: false,
+        imageRange: 6
+    }
+
+];
